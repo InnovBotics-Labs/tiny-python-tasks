6ad839f3262bcdb1f93ecc6a476fd34c1c3b8b6b
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2025-02-05 09:52:00 -0800

    fix: pyl

--- Tasks/REST Cafes/main.py
+++ Tasks/REST Cafes/main.py
@@ -10,12 +10,12 @@ pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
 '''
-
+import random as ran
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-
+from sqlalchemy.sql.functions import random
 
 app = Flask(__name__)
 
@@ -43,6 +43,21 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self)->dict:
+        """Convert sql alchemly into dic"""
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+        # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        #return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -55,6 +70,26 @@ def home():
 
 
 # HTTP GET - Read Record
+@app.route("/random", methods=["GET"])
+def get_random_cafe():
+    """ gets a random cafes """
+    result = db.session.execute(db.select(Cafe))
+    all_cafes= result.scalars().all()
+    random_cafe = ran.choice(all_cafes)
+    # return jsonify(cafe={
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #     "seats": random_cafe.seats,
+    #     "has_toilet": random_cafe.has_toilet,
+    #     "has_wifi": random_cafe.has_wifi,
+    #     "has_sockets": random_cafe.has_sockets,
+    #     "can_take_calls": random_cafe.can_take_calls,
+    #     "coffee_price": random_cafe.coffee_price,})
+    return jsonify(random_cafe.to_dict())
+
 
 # HTTP POST - Create Record
 
